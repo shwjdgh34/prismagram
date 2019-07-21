af6f36be56006f8649d2530a6df7e504eecf96fc
Jeongho Noh <nossi8128@gmail.com>
2019-07-21 09:49:06 +0900

    constructed addComment to post resolver. this also used 'connect' to create Comment! it would be helpful to construct deleteComment by self

---
+++ src/api/Comment/addComment/addComment.js
@@ -0,0 +1,26 @@
+import { isAutheticated } from '../../../middlewares';
+import { prisma } from '../../../../generated/prisma-client';
+
+export default {
+  Mutation: {
+    addComment: async (_, args, { request }) => {
+      isAutheticated(request);
+      const { text, postId } = args;
+      const { user } = request;
+      const comment = await prisma.createComment({
+        user: {
+          connect: {
+            id: user.id
+          }
+        },
+        post: {
+          connect: {
+            id: postId
+          }
+        },
+        text
+      });
+      return comment;
+    }
+  }
+};
